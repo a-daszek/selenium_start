561e2a58960a4f691ca3c0f5a9408962601fb89c
Anna Daszyńska <anna.daszynska@protonmail.com>
2023-04-06 13:44:31 +0200

    Initial Commit

--- main.py
+++ main.py
@@ -1,15 +1,70 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+import time
+
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
 
-driver.get('https://www.amazon.com/Logitech-LIGHTSPEED-Wireless-Gaming-Mouse/dp/B07CMS5Q6N/ref=sr_1_3?keywords=gaming%2Bmouse&pd_rd_r=63f31e37-7ca5-40bb-9826-b8b7ea282dcc&pd_rd_w=Bopxl&pd_rd_wg=SXcFo&pf_rd_p=12129333-2117-4490-9c17-6d31baf0582a&pf_rd_r=C6BR01C90S3BZG5QWFZ0&qid=1680774111&refinements=p_89%3ALogitech%2BG%7CSteelSeries%2Cp_n_feature_two_browse-bin%3A23473577011%2Cp_n_feature_eight_browse-bin%3A23627257011&rnid=23627253011&s=videogames&sr=1-3&th=1')
+driver.get('http://orteil.dashnet.org/experiments/cookie/')
 
-price = driver.find_element(By.CLASS_NAME, "a-offscreen+span")
-print(price.text)
+items = driver.find_elements(By.CSS_SELECTOR, "#store div")
+item_ids = [item.get_attribute("id") for item in items]
+
+ciastko = driver.find_element(By.ID, "cookie")
+
+timeout = time.time() + 5
+fiveteen_min = time.time() + 60 * 15
+
+while True:
+    ciastko.click()
+    if time.time() > timeout:
+
+        all_prices = driver.find_elements(By.CSS_SELECTOR, "#store b")
+        item_prices = []
+
+        for price in all_prices:
+            element_text = price.text
+            if element_text != "":
+                cost = int(element_text.split("-")[1].strip().replace(",", ""))
+                item_prices.append(cost)
+
+        cookie_upgrades = {}
+        for n in range(len(item_prices)):
+            cookie_upgrades[item_prices[n]] = item_ids[n]
+
+        # Get current cookie count
+        money_element = driver.find_element(By.ID, "money").text
+        if "," in money_element:
+            money_element = money_element.replace(",", "")
+        cookie_count = int(money_element)
+
+        # Find upgrades that we can currently afford
+        affordable_upgrades = {}
+        for cost, id in cookie_upgrades.items():
+            if cookie_count > cost:
+                affordable_upgrades[cost] = id
+
+        # Purchase the most expensive affordable upgrade
+        highest_price_affordable_upgrade = max(affordable_upgrades)
+        print(highest_price_affordable_upgrade)
+        to_purchase_id = affordable_upgrades[highest_price_affordable_upgrade]
+
+        driver.find_element(By.ID, to_purchase_id).click()
+
+        # Add another 5 seconds until the next check
+        timeout = time.time() + 5
+
+        # After 5 minutes stop the bot and check the cookies per second count.
+        if time.time() > fiveteen_min:
+            cookie_per_s = driver.find_element(By.ID, "cps").text
+            print(cookie_per_s)
+            break
+
+# price = driver.find_element(By.CLASS_NAME, "a-offscreen+span")
+# print(price.text)
 
 
